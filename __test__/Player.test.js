d24be1b5d7a37071941c04a9c23a461f309b5ba0
Aleph Null <[likhonhasankomol@gmail.com]>
2022-10-04 14:36:46 -0400

    Finished Player object and all of its functions

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -66,3 +66,30 @@ test("subtracts from player's health",() =>{
 
     expect(player.health).toBe(0);
 });
+
+test("gets player attack value",()=>{
+    const player = new Player('Mau');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('adds potion to the inventory',()=>{
+    const player = new Player('mau');
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+test('uses a potion from inventory',() =>{
+    const player = new Player('Mau');
+    player.inventory = [new Potion(),new Potion(),new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+
+    expect(player.inventory.length).toBeLessThan(oldCount);
+});
